94cc0488285af5acb16137a152b716274750d1a8
ipin0917826334 <71114672+ipin0917826334@users.noreply.github.com>
2020-11-13 12:08:19 +0700

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'panwit.mysql.database.azure.com', 'panwit@panwit', '123456789', 'itflab', 3306);
+mysqli_real_connect($conn, 'pppntst.mysql.database.azure.com', 'ppntst@ppntst', 'Asd04140', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
